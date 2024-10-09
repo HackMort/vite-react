baadb62cb2b12c83dc0dad6315e7557f5f4c8e7d
Jonathan Juarez <jonathan.juarezwp@gmail.com>
2024-10-09 13:57:24 -0600

    build: :rocket: Add Vite Legacy Plugin
    Add Vite Legacy Plugin for better browser compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 import packageVersion from 'vite-plugin-package-version'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), packageVersion()]
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    }),
+    packageVersion()
+  ]
 })
