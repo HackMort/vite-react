32d6f780a4f6b5357b7418d45ed9c210bb7a2fcd
Jonathan Juarez <jonathan.juarezwp@gmail.com>
2024-10-09 13:45:01 -0600

    Add Initial Project Files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { version } from "./version.js";
+
+function App() {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <p>Version: {version}</p>
+    </div>
+  );
+}
+
+export default App;
